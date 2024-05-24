534b04f23788bdeae4cc1c6eaec69b354fd12e0e
Kohki-Takatama <koh.takatama@gmail.com>
2024-05-24 18:43:59 +0900

    tab機能を実装

--- public/script.js
+++ public/script.js
@@ -1,5 +1,22 @@
-const btn = document.getElementById('btn1');
+document.querySelectorAll('[role="tab"]').forEach((tab) => {
+  tab.addEventListener('click', (event) => {
+    // Hide all tab contents
+    document.querySelectorAll('.tab-content').forEach((content) => {
+      content.classList.remove('active');
+    });
 
-btn.addEventListener('click', () => {
-  console.log('ok');
+    // Deactivate all tabs
+    document.querySelectorAll('[role="tab"]').forEach((tab) => {
+      tab.classList.remove('tab-active');
+    });
+
+    // Activate clicked tab
+    event.currentTarget.classList.add('tab-active');
+
+    // Show corresponding tab content
+    const selectedTab = event.currentTarget.getAttribute('for');
+    document
+      .getElementById(`tab_content_${selectedTab.split('_').pop()}`)
+      .classList.add('active');
+  });
 });
